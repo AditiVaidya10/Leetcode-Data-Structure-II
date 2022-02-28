a1251c87389061f0a63b315bdb27a3a7197af809
Aditi Atul Vaidya <61463717+AditiVaidya10@users.noreply.github.com>
2022-02-28 09:24:33 +0530

    Create Group Anagrams.py

---
+++ Group Anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+        
